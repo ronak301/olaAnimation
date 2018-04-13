c1cb28ba34da07b3a80424afa1452a64a2c7e24b
Ronak Kothari <ronak.kothari@phonepe.com>
2018-04-14 00:14:24 +0530

    first step completed

--- FirstView.js
+++ FirstView.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { View, Animated, Dimensions } from 'react-native'
+import { View, Animated, Dimensions, Image, Easing } from 'react-native'
 const { width, height } = Dimensions.get('window')
 
+const animCount = 0
 export default class extends React.Component {
 
   constructor() {
     super()
     this.state = {
-      anim: new Animated.Value(0)
+      anim: new Animated.Value(0),
+      anim2: new Animated.Value(0)
     }
   }
 
@@ -16,25 +18,49 @@ export default class extends React.Component {
   }
 
   startAnimation = () => {
+    if (animCount === 3) {
+      this.secondAnimation()
+      return 
+    }
+    animCount++;
+    this.state.anim.setValue(0)
     Animated.timing(this.state.anim, {
-      duration: 5000,
-      toValue: 1
+      duration: 1000,
+      toValue: 1,
+      easing: Easing.elastic(0.04),
+    }).start(this.startAnimation)
+  }
+
+  secondAnimation = () => {
+    this.state.anim.setValue(2)
+    Animated.timing(this.state.anim, {
+      duration: 1000,
+      toValue: 3,
+      easing: Easing.elastic(0.04),
+      useNativeDriver: true
     }).start()
   }
 
 
   render() {
-    const borderWidth = this.state.anim.interpolate({
-      inputRange: [0, 1],
-      outputRange: [400, 0],
+    const translateY = this.state.anim.interpolate({
+      inputRange: [0, 0.5, 1, 2, 2.95, 3],
+      outputRange: [0, -40, 0, 0, -150, 70],
+      extrapolate: 'clamp'
     })
     const opacity = this.state.anim.interpolate({
-      inputRange: [0, 1],
-      outputRange: [1, 0],
+      inputRange: [2, 2.999, 3],
+      outputRange: [1, 1, 0],
+      extrapolate: 'clamp'
     })
     return (
-      <Animated.View style={{ alignItems: 'center', justifyContent: 'center', position: 'absolute', width, height, top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'transparent' }} >
-        <Animated.View style={{ opacity, borderWidth, borderColor: 'white', width: 1000, height: 1000, borderRadius: 500, backgroundColor: 'red' }}></Animated.View>
+      <Animated.View style={{ opacity, alignItems: 'center', justifyContent: 'center', position: 'absolute', width, height, top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'white' }} >
+        <Animated.Image
+          style={{
+            transform: [{ translateY }],
+            flexShrink: 1, marginBottom: 200, overflow: 'hidden'
+          }}
+         reszieMode={'contain'} source={require('./point.png')} />
       </Animated.View>
     )
   }
